8e55b493f57fc4d9598b60881e9719b466cd20d6
Aaron Kim <aaronkim1205@gmail.com>
2016-09-27 15:34:47 -0700

    Create a react todo app

---
+++ src/components/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TodosLIst from './todos-list';
+
+const todos = [
+{
+    task: 'make React tutorial',
+    isCompleted: false
+},
+{
+    task: 'eat dinner',
+    isCompleted: true
+}
+];
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>React ToDos App</h1>
+        <TodosLIst />
+      </div>
+    );
+  }
+}

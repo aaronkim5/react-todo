ae0b98012fdf5fcbc34200fa8bfe8415cbb3eb27
Aaron Kim <aaronkim1205@gmail.com>
2016-09-28 10:53:41 -0700

    Create list of items and buttons

--- src/components/app.js
+++ src/components/app.js
@@ -3,21 +3,29 @@ import TodosLIst from './todos-list';
 
 const todos = [
 {
-    task: 'make React tutorial',
+    task: 'Make React tutorial',
     isCompleted: false
 },
 {
-    task: 'eat dinner',
+    task: 'Eat dinner',
     isCompleted: true
 }
 ];
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>React ToDos App</h1>
-        <TodosLIst />
+        <h1>Aarons ToDos App</h1>
+        <TodosLIst todos={this.state.todos}/>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel']
+        loaders: ['babel']
       }
     ]
   },

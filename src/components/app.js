9d40c78a65ab669e9d883fadd7a7ef5c5545c9bb
Aaron Kim <aaronkim1205@gmail.com>
2016-09-28 14:14:05 -0700

    Create components to build a todo list, all functions working, crud and validations

--- src/components/app.js
+++ src/components/app.js
@@ -26,11 +26,22 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>Aarons ToDos App</h1>
-        <CreateTodo createTask={this.createTask.bind(this)} />
+        <CreateTodo todos={this.state.todos} createTask={this.createTask.bind(this)} />
         <TodosLIst
-          todos={this.state.todos}/>
+          todos={this.state.todos}
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)}
+          deleteTask={this.deleteTask.bind(this)}
+          />
       </div>
     );
+  }
+
+  toggleTask(task) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+    foundTodo.isCompleted = !foundTodo.isCompleted;
+    this.setState({ todos: this.state.todos });
+
   }
 
   createTask(task){
@@ -38,6 +49,17 @@ export default class App extends React.Component {
       task,
       isCompleted: false
     });
+    this.setState({ todos: this.state.todos });
+  }
+
+  saveTask(oldTask, newTask){
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+    foundTodo.task = newTask;
+    this.setState({ todos: this.state.todos });
+  }
+
+  deleteTask(taskToDelete){
+    _.remove(this.state.todos, todo => todo.task === taskToDelete);
     this.setState({ todos: this.state.todos });
   }
 }
--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -5,7 +5,9 @@ import TodosListItem from './todos-list-items';
 
 export default class TodosList extends React.Component {
   renderItems() {
-    return _.map(this.props.todos, (todo, index)=> <TodosListItem key={index} {...todo} />);
+    const props = _.omit(this.props, 'todos');
+
+    return _.map(this.props.todos, (todo, index)=> <TodosListItem key={index} {...todo} {...props} />);
   }
 
 
